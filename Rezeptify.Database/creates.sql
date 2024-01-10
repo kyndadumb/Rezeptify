657d0e358d76e6ed4d973337c7508394e3a0e8a7
Florian Bennecker <f.bennecker@bennecker.eu>
2024-01-10 13:26:16 +0100

    Datenbanken: Update der Struktur fixes #3

--- Rezeptify.Database/creates.sql
+++ Rezeptify.Database/creates.sql
@@ -1,7 +1,30 @@
-create table ingredients (id integer primary key, name text not null, quantity float);
+CREATE TABLE "ingredients" (
+	"id" INTEGER NOT NULL,
+	"name" TEXT NOT NULL,
+	"quantity" REAL NULL,
+	"unit" VARCHAR(50) NULL,
+	PRIMARY KEY ("id")
+);
 
-create table recipe (id integer primary key, instructions text);
 
-create table recipeingredients (recipe_id integer not null, ingredient_id integer not null,
-FOREIGN KEY(recipe_id) REFERENCES recipe(id)
-FOREIGN KEY(ingredient_id) REFERENCES ingredients(id));
+CREATE TABLE "recipe" (
+	"id" INTEGER NOT NULL,
+	"instructions" TEXT NULL,
+	"name" TEXT NULL,
+	PRIMARY KEY ("id")
+);
+
+
+CREATE TABLE "recipeingredients" (
+	"recipe_id" INTEGER NOT NULL,
+	"ingredient_id" INTEGER NOT NULL,
+	CONSTRAINT "0" FOREIGN KEY ("ingredient_id") REFERENCES "ingredients" ("id") ON UPDATE NO ACTION ON DELETE NO ACTION,
+	CONSTRAINT "1" FOREIGN KEY ("recipe_id") REFERENCES "recipe" ("id") ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+CREATE TABLE "eancodes" (
+	"id" INTEGER NOT NULL,
+	"eancode" VARCHAR(255) NOT NULL,
+	"ingredient_id" INTEGER NOT NULL,
+	CONSTRAINT "0" FOREIGN KEY ("ingredient_id") REFERENCES "ingredients" ("id") ON UPDATE NO ACTION ON DELETE NO ACTION
+);
