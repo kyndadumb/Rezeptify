eb0d9fb6587fdf4c4f991ddde3198f82ccf6e5fc
TheSolas <solas12345@googlemail.com>
2024-01-15 11:04:01 +0100

    Font für Symbole, FileManagerimplementierung

---
+++ Rezeptify/FileManager.cs
@@ -0,0 +1,17 @@
+﻿using Rezeptify.VM.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rezeptify
+{
+    public class FileManager : IFileManager
+    {
+        public string GetApplicationFolder()
+        {
+            return FileSystem.AppDataDirectory;
+        }
+    }
+}
