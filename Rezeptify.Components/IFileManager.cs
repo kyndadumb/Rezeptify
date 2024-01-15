e947cbe74ec3b3d685cb397c7644dbc257524e92
TheSolas <solas12345@googlemail.com>
2024-01-15 11:02:47 +0100

    IFileManager Service

---
+++ Rezeptify.Components/IFileManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rezeptify.AppComponents;
+
+public interface IFileManager
+{
+    public string GetApplicationFolder();
+}
