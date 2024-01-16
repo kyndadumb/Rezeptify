6413d0688e7f525f6458d5280de9cb4d626e55ce
TheSolas <solas12345@googlemail.com>
2024-01-16 09:25:59 +0100

    Converter für Mengenanzeige

---
+++ Rezeptify/ShowMengeConverter.cs
@@ -0,0 +1,26 @@
+﻿using Rezeptify.AppComponents.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rezeptify
+{
+    public class ShowMengeConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            var ingr = (Ingredients)value;
+            if(ingr == null) return "-";
+            if(String.IsNullOrEmpty(ingr.Unit)) return ingr.Quantity;
+            return $"{ingr.Quantity} {ingr.Unit}";
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
